1d5d7776f8ffa454fb976f11a583f5c5a63b1821
DaDarkWizard <dtmasker@mtu.edu>
2020-12-08 17:56:21 -0500

    yeet

---
+++ InstructorDashboard.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+	$config = parse_ini_file("db.ini");
+	$professor = parse_ini_file("website.ini")['professor'];
+
+	$dbh = new PDO($config['dsn'], $config['username'],$config['password']);
+
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	if(!isset($_SESSSION["TOKEN_DATA"]))
+	{
+	
+	}
+
+	$_SESSION["redirect"];
+	$_SESSION["login_task"];
+
+
+
+
+?>
